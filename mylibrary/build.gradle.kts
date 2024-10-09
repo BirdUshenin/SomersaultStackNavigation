88e83133957ec10d75ac9de03b149238a29b87e5
ilya <uiv@coolclever.ru>
2024-10-09 23:33:37 +0300

    version-v4-8

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("maven") {
+            create<MavenPublication>("release") {
                 from(components["release"])
                 groupId = "com.github.birdushenin"
                 artifactId = "somersaultstacknavigation"
