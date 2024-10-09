cd919b2c8350d9520b9d84fb28273cc1565daa87
ilya <uiv@coolclever.ru>
2024-10-09 12:58:05 +0300

    more like

--- app/src/main/java/com/ilyaushenin/somersaultstacknavigation/StatesModal.kt
+++ app/src/main/java/com/ilyaushenin/somersaultstacknavigation/StatesModal.kt
@@ -1,23 +1,28 @@
 package com.ilyaushenin.somersaultstacknavigation
 
 data class StatesModal(
-//    val somersaultStackNavigation: SomersaultStackNavigation<Any>
-//    = SomersaultStackNavigation(),
-    val somersaultStackNavigation: SomersaultStackNavigation<ScreenState>
+    val somersaultStackNavigation: SomersaultStackNavigation<Any>
     = SomersaultStackNavigation(),
-
 
     val x: Boolean = true
 )
+
+object MainScreens {
+    class BoxAState
+    class BoxBState
+}
+
+
 
 //data class ScreenState(
 //    val screenType: String,
 //    val args: Map<String, Any> = emptyMap()
 //)
 
-enum class ScreenState {
-    A,
-    B,
-    D,
-    L
-}
+//enum class ScreenState {
+//    A,
+//    B,
+//    D,
+//    L
+//}
+
