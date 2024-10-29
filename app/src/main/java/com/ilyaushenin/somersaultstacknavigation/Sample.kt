6782840dc3b5ad52a3016616addc1c917aeaccb6
Ilya Ushenin <ilyaushenin@gmail.com>
2024-10-29 16:25:03 +0300

    bottom-navigation

--- app/src/main/java/com/ilyaushenin/somersaultstacknavigation/Sample.kt
+++ app/src/main/java/com/ilyaushenin/somersaultstacknavigation/Sample.kt
@@ -1,15 +1,34 @@
 package com.ilyaushenin.somersaultstacknavigation
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ilyaushenin.somersault.NavigationScreensGraph
 import com.ilyaushenin.somersault.ScreenRegistry
 import com.ilyaushenin.somersault.SomersaultStackNavigation
+import com.ilyaushenin.somersaultstacknavigation.decorations.ParkingPost
 
 @Composable
 fun AppContent(
@@ -17,16 +36,64 @@ fun AppContent(
 ) {
     // Your state class with implementation SomersaultStackNavigation
     val stackNav = remember { statesModal.somersaultStackNavigation }
-
-    // You need to register a Navigation Graph in any main compose function
     RegisterScreens()
+
+    Column (modifier = Modifier){
+        Box(modifier = Modifier
+            .size(760.dp)
+            .background(Color.DarkGray),
+        ){
+            CompositionLocalProvider(SomersaultStackNavigation.LocalStackNav provides stackNav) {
+                // Pass the NavigationHandler in CompositionProvider
+                NavigationHandler()
+            }
+        }
+
+        BlockD(
+            stackNav = stackNav
+        )
+    }
+    // You need to register a Navigation Graph in any main compose function
+
 
     // This is done for the correct use of CompositionLocalProvider.
     // In the CompositionLocalProvider you need to declare LocalStackNav.
     // It is necessary to support current instance anywhere in the composition tree.
-    CompositionLocalProvider(SomersaultStackNavigation.LocalStackNav provides stackNav) {
-        // Pass the NavigationHandler in CompositionProvider
-        NavigationHandler()
+
+
+}
+
+
+@Composable
+@Preview
+fun Preview(){
+//    BlockD(
+//        stackNav = SomersaultStackNavigation("ParkingPlace")
+//    )
+
+}
+
+@Composable
+fun BlockD(
+    stackNav: SomersaultStackNavigation<String>
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray),
+        Arrangement.SpaceBetween
+    ) {
+        Button(onClick = {
+            stackNav.onForwardFlip("BlockC")
+        }) { Text("C") }
+
+        Button(onClick = {
+            stackNav.onForwardFlip("BlockA")
+        }) { Text("A") }
+
+        Button(onClick = {
+            stackNav.onForwardFlip("BlockB")
+        }) { Text("B") }
     }
 }
 
--- mylibrary/src/main/java/com/ilyaushenin/somersault/SomersaultStackNavigation.kt
+++ mylibrary/src/main/java/com/ilyaushenin/somersault/SomersaultStackNavigation.kt
@@ -1,102 +1,102 @@
-package com.ilyaushenin.somersault
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.compositionLocalOf
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-
-class SomersaultStackNavigation<T>(initialState: T) {
-
-    /**
-     * Represents the navigation stack.
-     * It stores a list of states (screens) as a [MutableStateFlow],
-     * which allows tracking stack changes in a reactive manner
-     * and automatically updating the UI when changes occur.
-     */
-    val navigationStack: StateFlow<List<T>> get() = _navigationStack
-    private val _navigationStack = MutableStateFlow(listOf(initialState))
-
-    /**
-     * Navigates forward through the app's screens.
-     * It can be used to navigate to a new screen in the application by adding the corresponding
-     * state to the navigation stack.
-     */
-    fun onForwardFlip(state: T) {
-        _navigationStack.value += state
-    }
-
-    /**
-     * Navigates back through the app's screens.
-     * It can be used to navigate back in the application and update the UI according to
-     * the current state of the navigation stack.
-     */
-    fun onBackFlip(): T? {
-        if (_navigationStack.value.size > 1) {
-            _navigationStack.value = _navigationStack.value.dropLast(1)
-        }
-        return currentState()
-    }
-
-    /**
-     * Get the current state in the navigation stack.
-     * It can be used to get the current state and display it on the screen.
-     */
-    private fun currentState(): T? = _navigationStack.value.lastOrNull()
-
-    companion object {
-        /**
-         * Provides access to [SomersaultStackNavigation] instance anywhere in the composition tree.
-         */
-        val LocalStackNav = compositionLocalOf<SomersaultStackNavigation<*>> {
-            error("StackNav not provided")
-        }
-        inline fun <reified T> getNavigation(stackNav: SomersaultStackNavigation<*>)
-                : SomersaultStackNavigation<T> {
-            return stackNav as? SomersaultStackNavigation<T>
-                ?: error("Expected SomersaultStackNavigation<${T::class.java}> but found different type")
-        }
-    }
-
-    /**
-     * Determines whether it is possible to go back to the previous screen.
-     */
-    fun canGoBack(): Boolean = _navigationStack.value.size > 1
-}
-
-object ScreenRegistry {
-    /**
-     * These methods are responsible for registering your screens that you will create.
-     */
-    private val screens = mutableMapOf<String, @Composable (
-        SomersaultStackNavigation<String>
-    ) -> Unit>()
-
-    fun screen(key: String, content: @Composable (SomersaultStackNavigation<String>) -> Unit) {
-        screens[key] = content
-    }
-
-    fun getScreen(key: String): (@Composable (SomersaultStackNavigation<String>) -> Unit)? {
-        return screens[key]
-    }
-}
-
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class NavigationScreensGraph
-/**
- * Create methods to your @Compose function
- *
- * ```
- * fun registerScreens() {
- *     val screens: List<Pair<String, @Composable (SomersaultStackNavigation<T>) -> Unit>> = listOf(
- *         "BoxA" to { stackNav -> BoxA("This is Box A", stackNav) },
- *         "BoxB" to { stackNav -> BoxB("This is Box B", stackNav) },
- *         "BoxC" to { stackNav -> BoxC("This is Box C", stackNav) }
- *     )
- *
- *     screens.forEach { (key, screenContent) ->
- *         ScreenRegistry.screen(key, screenContent)
- *     }
- * }
- * ```
- */
+//package com.ilyaushenin.somersault
+//
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.compositionLocalOf
+//import kotlinx.coroutines.flow.MutableStateFlow
+//import kotlinx.coroutines.flow.StateFlow
+//
+//class SomersaultStackNavigation<T>(initialState: T) {
+//
+//    /**
+//     * Represents the navigation stack.
+//     * It stores a list of states (screens) as a [MutableStateFlow],
+//     * which allows tracking stack changes in a reactive manner
+//     * and automatically updating the UI when changes occur.
+//     */
+//    val navigationStack: StateFlow<List<T>> get() = _navigationStack
+//    private val _navigationStack = MutableStateFlow(listOf(initialState))
+//
+//    /**
+//     * Navigates forward through the app's screens.
+//     * It can be used to navigate to a new screen in the application by adding the corresponding
+//     * state to the navigation stack.
+//     */
+//    fun onForwardFlip(state: T) {
+//        _navigationStack.value += state
+//    }
+//
+//    /**
+//     * Navigates back through the app's screens.
+//     * It can be used to navigate back in the application and update the UI according to
+//     * the current state of the navigation stack.
+//     */
+//    fun onBackFlip(): T? {
+//        if (_navigationStack.value.size > 1) {
+//            _navigationStack.value = _navigationStack.value.dropLast(1)
+//        }
+//        return currentState()
+//    }
+//
+//    /**
+//     * Get the current state in the navigation stack.
+//     * It can be used to get the current state and display it on the screen.
+//     */
+//    private fun currentState(): T? = _navigationStack.value.lastOrNull()
+//
+//    companion object {
+//        /**
+//         * Provides access to [SomersaultStackNavigation] instance anywhere in the composition tree.
+//         */
+//        val LocalStackNav = compositionLocalOf<SomersaultStackNavigation<*>> {
+//            error("StackNav not provided")
+//        }
+//        inline fun <reified T> getNavigation(stackNav: SomersaultStackNavigation<*>)
+//                : SomersaultStackNavigation<T> {
+//            return stackNav as? SomersaultStackNavigation<T>
+//                ?: error("Expected SomersaultStackNavigation<${T::class.java}> but found different type")
+//        }
+//    }
+//
+//    /**
+//     * Determines whether it is possible to go back to the previous screen.
+//     */
+//    fun canGoBack(): Boolean = _navigationStack.value.size > 1
+//}
+//
+//object ScreenRegistry {
+//    /**
+//     * These methods are responsible for registering your screens that you will create.
+//     */
+//    private val screens = mutableMapOf<String, @Composable (
+//        SomersaultStackNavigation<String>
+//    ) -> Unit>()
+//
+//    fun screen(key: String, content: @Composable (SomersaultStackNavigation<String>) -> Unit) {
+//        screens[key] = content
+//    }
+//
+//    fun getScreen(key: String): (@Composable (SomersaultStackNavigation<String>) -> Unit)? {
+//        return screens[key]
+//    }
+//}
+//
+//@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+//@Retention(AnnotationRetention.RUNTIME)
+//annotation class NavigationScreensGraph
+///**
+// * Create methods to your @Compose function
+// *
+// * ```
+// * fun registerScreens() {
+// *     val screens: List<Pair<String, @Composable (SomersaultStackNavigation<T>) -> Unit>> = listOf(
+// *         "BoxA" to { stackNav -> BoxA("This is Box A", stackNav) },
+// *         "BoxB" to { stackNav -> BoxB("This is Box B", stackNav) },
+// *         "BoxC" to { stackNav -> BoxC("This is Box C", stackNav) }
+// *     )
+// *
+// *     screens.forEach { (key, screenContent) ->
+// *         ScreenRegistry.screen(key, screenContent)
+// *     }
+// * }
+// * ```
+// */
